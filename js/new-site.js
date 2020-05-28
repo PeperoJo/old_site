3221789bf3df708e117d29c982c96d834002fdce
PeperoJo <hanjoseph11@gmail.com>
2020-05-27 21:40:07 -0400

    filled in projects

--- js/new-site.js
+++ js/new-site.js
@@ -55,11 +55,11 @@ for (var i = 0; i < btns.length; i++) {
 
 
 // scroll
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+// window.onscroll = function() {myFunction()};
+//
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
