02602003f6d75e516abf18c0546420a854be298d
PeperoJo <hanjoseph11@gmail.com>
2020-05-27 20:19:07 -0400

    scroll bar

--- new_site/filter.js
+++ new_site/filter.js
@@ -52,3 +52,14 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+// scroll
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
